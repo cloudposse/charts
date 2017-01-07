b2713ad42ad2b4aeaa58f5921426c21f24515a39
Erik Osterman <e@osterman.com>
2017-01-07 14:15:10 -0800

    add all repos

--- contrib/configure-dev.sh
+++ contrib/configure-dev.sh
@@ -2,12 +2,12 @@
 CHART_URL=https://charts.dev.cloudposse.com
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 ## configure stable repo
-helm repo rm cloudposse-stable 2>/dev/null
-helm repo add cloudposse-stable  ${CHART_URL}/${BRANCH}/stable/
+helm repo rm cloudposse-dev-stable 2>/dev/null
+helm repo add cloudposse-dev-stable  ${CHART_URL}/${BRANCH}/stable/
 
 ## configure incubator repo
-helm repo rm cloudposse-incubator 2>/dev/null
-helm repo add cloudposse-incubator  https://${CHART_URL}/${BRANCH}/incubator/
+helm repo rm cloudposse-dev-incubator 2>/dev/null
+helm repo add cloudposse-dev-incubator  https://${CHART_URL}/${BRANCH}/incubator/
 
 ## Update index
 helm repo update
