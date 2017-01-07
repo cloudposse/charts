94eb84e42315d41039028e6be975c1e3a66c23dd
Erik Osterman <e@osterman.com>
2017-01-07 14:11:04 -0800

    Update chart urls

--- contrib/configure-dev.sh
+++ contrib/configure-dev.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
+CHART_URL=https://charts.dev.cloudposse.com
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 ## configure stable repo
 helm repo rm cloudposse-stable 2>/dev/null
-helm repo add cloudposse-stable  https://cloudposse-dev-charts.s3.amazonaws.com/$BRANCH/stable/
+helm repo add cloudposse-stable  ${CHART_URL}/${BRANCH}/stable/
 
 ## configure incubator repo
 helm repo rm cloudposse-incubator 2>/dev/null
-helm repo add cloudposse-incubator  https://cloudposse-dev-charts.s3.amazonaws.com/$BRANCH/incubator/
+helm repo add cloudposse-incubator  https://${CHART_URL}/${BRANCH}/incubator/
+
+## Update index
+helm repo update
 
 ## display configured repos
 helm repo list
--- contrib/configure.sh
+++ contrib/configure.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
+CHART_URL=https://charts.cloudposse.com
+
 ## configure stable repo
 helm repo rm cloudposse-stable 2>/dev/null
-helm repo add cloudposse-stable  https://cloudposse-charts.s3.amazonaws.com/stable/
+helm repo add cloudposse-stable  ${CHART_URL}/stable
 
 ## configure incubator repo
 helm repo rm cloudposse-incubator 2>/dev/null
-helm repo add cloudposse-incubator  https://cloudposse-charts.s3.amazonaws.com/incubator/
+helm repo add cloudposse-incubator  ${CHART_URL}/incubator
+
+## Update index
+helm repo update
 
 ## display configured repos
 helm repo list
