d5bd9a9a98338dac268279048829adbbf581fa13
Erik Osterman <e@osterman.com>
2017-01-09 12:43:55 -0800

    Do a better job guessing branch name

--- contrib/configure-dev.sh
+++ contrib/configure-dev.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 REPO_URL=${REPO_URL:-https://charts.dev.cloudposse.com}
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ -n "${TRAVIS_PULL_REQUEST_BRANCH}" ]; then
+  BRANCH="${TRAVIS_PULL_REQUEST_BRANCH}"
+elif [ -n "${TRAVIS_BRANCH}" ]; then
+  BRANCH="${TRAVIS_BRANCH}"
+elif [ -n "${TRAVIS_TAG}" ]; then
+  BRANCH="${TRAVIS_TAG}"
+else
+  BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+fi
+
 ## configure stable repo
 helm repo rm cloudposse-dev-stable 2>/dev/null
-helm repo add cloudposse-dev-stable ${REPO_URL}/${BRANCH}/stable/
+helm repo add cloudposse-dev-stable ${REPO_URL}/${BRANCH}/stable
 
 ## configure incubator repo
 helm repo rm cloudposse-dev-incubator 2>/dev/null
-helm repo add cloudposse-dev-incubator https://${REPO_URL}/${BRANCH}/incubator/
+helm repo add cloudposse-dev-incubator ${REPO_URL}/${BRANCH}/incubator
 
 ## Update index
 helm repo update
